062926c5f41bd28f58a6ca6426c3f7fd5eb2d619
legitbeep <glorytoharsh@gmail.com>
2021-11-20 23:46:24 +0530

    add precommit/postcommit lint

--- src/routes.ts
+++ src/routes.ts
@@ -1,12 +1,12 @@
-import { RouteProps } from 'react-router-dom';
+import { RouteProps } from "react-router-dom";
 
-import Home from './components/home';
+import Home from "./components/home";
 
-export const routes:Array<RouteProps> = [
-    { 
-        path : '/',
-        element : Home(),
-    }
-] ;
+export const routes: Array<RouteProps> = [
+  {
+    path: "/",
+    element: Home(),
+  },
+];
 
 export const privateRoutes: Array<RouteProps> = [];
--- src/styles/customTheme.ts
+++ src/styles/customTheme.ts
@@ -10,6 +10,6 @@ export const theme = extendTheme({
     // }
   },
   config: {
-    initialColorMode:  "light",
+    initialColorMode: "light",
   },
 });
