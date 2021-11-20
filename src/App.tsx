43935f44a85d1de99e605fbf2e0dc377f306e1f1
legitbeep <glorytoharsh@gmail.com>
2021-11-20 17:24:41 +0530

    add routes and components

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,26 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import { useState, lazy } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-function App() {
-  const [count, setCount] = useState(0)
+// import Layout from "components/layout";
+// import PrivateRoute from "components/route/PrivateRoute";
+import NotFound from "components/404";
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
-    </div>
-  )
-}
+import { privateRoutes, routes } from "routes";
 
-export default App
+const App = () => (
+  <BrowserRouter>
+    {/* <Layout> */}
+    <Routes>
+      {routes.map((routeProps, index) => (
+        <Route exact {...routeProps} key={index} />
+      ))}
+      {/* {privateRoutes.map((privateRouteProps, index) => (
+          <PrivateRoute {...privateRouteProps} key={`privateRoute-${index}`} />
+        ))} */}
+      <Route element={<NotFound />} />
+    </Routes>
+    {/* </Layout> */}
+  </BrowserRouter>
+);
+
+export default App;
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import reactRefresh from '@vitejs/plugin-react-refresh';
+import { resolve } from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [reactRefresh()],
+  esbuild: {
+    jsxInject : `import React from 'react'`
+  },
+  resolve: {
+    alias: {
+      components: resolve(__dirname,"src/components"),
+      styles: resolve(__dirname, "src/styles"),
+      routes: resolve(__dirname, "src/routes"),
+    }
+  }
 })
