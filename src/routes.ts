18dc74c5cf342bf2c85108e6d51f81fb4d9aa696
legitbeep <glorytoharsh@gmail.com>
2021-11-22 17:11:30 +0530

    fix commitlint

--- src/routes.ts
+++ src/routes.ts
@@ -3,6 +3,10 @@ import { RouteProps } from "react-router-dom";
 import Home from "./components/home";
 
 export const routes: Array<RouteProps> = [
+  {
+    path: "/",
+    element: Home(),
+  },
 ];
 
 export const privateRoutes: Array<RouteProps> = [];
