cc418f49e78f41b2a0252bac679c7a06f1c02778
legitbeep <glorytoharsh@gmail.com>
2021-11-22 17:05:23 +0530

    test commit

--- src/routes.ts
+++ src/routes.ts
@@ -3,10 +3,6 @@ import { RouteProps } from "react-router-dom";
 import Home from "./components/home";
 
 export const routes: Array<RouteProps> = [
-  {
-    path: "/",
-    element: Home(),
-  },
 ];
 
 export const privateRoutes: Array<RouteProps> = [];
